7d419b9777b946fc533cd72f859f342e098c111d
Kovtun Bogdan <kowtun.bogdan2017@yandex.ru>
2020-12-16 17:22:59 +0200

    update

--- Templates/src/checkout/Offer.java
+++
@@ -1,5 +0,0 @@
-package checkout;
-
-public abstract class Offer {
-    public abstract void apply(Check check);
-}
---
+++ oop-workshop-master/src/checkout/FactorByCategoryOffer.java
@@ -0,0 +1,16 @@
+package checkout;
+
+public class FactorByCategoryOffer extends Offer {
+    final Category category;
+    final int factor;
+
+    public FactorByCategoryOffer(Category category, int factor) {
+        this.category = category;
+        this.factor = factor;
+    }
+
+    @Override
+    public void apply(Check check) {
+
+    }
+}
